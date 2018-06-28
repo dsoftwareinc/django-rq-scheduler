52fa2462053c9808f5c922feb2431fa6d8e08c29
tom-price <tom@tomprice.ca>
2018-06-28 19:37:58 -0400

    MIDDLEWARE_CLASSES was renamed to MIDDLEWARE due to change after 1.10
    And SessionAuthenticationMiddleware class is removed in 2.0 as it
    provided no functionality since session authentication is unconditionally
    enabled in Django 1.10.

--- testproject19/testproject19/settings.py
+++ testproject19/testproject19/settings.py
@@ -41,13 +41,12 @@ INSTALLED_APPS = [
     'scheduler',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -78,10 +77,8 @@ WSGI_APPLICATION = 'testproject19.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'testproject',
-        'USER': 'ubuntu',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
