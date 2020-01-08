901687fe27d241fa21f10838b3f1caffce258054
Marc Laughton <info@marclaughton.com>
2020-01-08 14:27:51 -0500

    Update setup py for pypi distribution (#26)
    * Add runtime for Python deployment and build tools, update Python version for Circle config
    
    * Update setup.py for more accurate PyPi info, test_suite path, and current package maintainer contact info
    
    * Isolate PR changes
    
    * 2.7.6 to 2.7.9 per master

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 import os
 from distutils.core import setup
+
 from setuptools import find_packages
 
 
@@ -14,8 +15,8 @@ def long_desc(root_path):
                 yield f.read()
 
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-long_description = "\n\n".join(long_desc(HERE))
+PATH_OF_RUNNING_SCRIPT = os.path.abspath(os.path.dirname(__file__))
+long_description = "\n\n".join(long_desc(PATH_OF_RUNNING_SCRIPT))
 
 
 def get_version(root_path):
@@ -26,20 +27,19 @@ def get_version(root_path):
 
 
 tests_require = [
-    'factory_boy>=2.6.1',
-    'psycopg2>=2.6.1',
+    'factory_boy>=2.11.1',
 ]
 
 
 setup(
     name='django-rq-scheduler',
-    version=get_version(HERE),
+    version=get_version(PATH_OF_RUNNING_SCRIPT),
     description='A database backed job scheduler for Django RQ',
     long_description=long_description,
     packages=find_packages(),
     include_package_data=True,
-    author='Chad Shryock',
-    author_email='chad@isl.co',
+    author='ISL',
+    author_email='dev@isl.co',
     url='https://github.com/istrategylabs/django-rq-scheduler',
     zip_safe=True,
     install_requires=[
@@ -47,10 +47,11 @@ setup(
         'django-model-utils>=2.4.0',
         'django-rq>=0.9.3',
         'rq-scheduler>=0.6.0',
-        'pytz>=2015.7',
+        'pytz>=2018.5',
         'croniter>=0.3.24',
     ],
     tests_require=tests_require,
+    test_suite='scheduler.tests',
     extras_require={
         'test': tests_require,
     },
@@ -62,6 +63,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
         'Framework :: Django',
     ],
 )
