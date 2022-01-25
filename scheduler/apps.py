0e639cad5869722fa18a0a4813de95649d1f40a2
Daniel M <cunla@users.noreply.github.com>
2022-01-25 12:27:00 -0500

    Version 2022.2

--- scheduler/apps.py
+++ scheduler/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
-from django.db.models.functions import Now
 from django.utils.translation import gettext_lazy as _
 
 
@@ -9,31 +8,17 @@ class SchedulerConfig(AppConfig):
 
     def ready(self):
         try:
-            self.reschedule_cron_jobs()
-            self.reschedule_repeatable_jobs()
-            self.reschedule_scheduled_jobs()
+            self.reschedule_jobs('ScheduledJob')
+            self.reschedule_jobs('CronJob')
+            self.reschedule_jobs('RepeatableJob')
         except:
             # Django isn't ready yet, example a management command is being
             # executed
             pass
 
-    def reschedule_cron_jobs(self):
-        CronJob = self.get_model('CronJob')
-        jobs = CronJob.objects.filter(enabled=True)
-        self.reschedule_jobs(jobs)
-
-    def reschedule_repeatable_jobs(self):
-        RepeatableJob = self.get_model('RepeatableJob')
-        jobs = RepeatableJob.objects.filter(enabled=True)
-        self.reschedule_jobs(jobs)
-
-    def reschedule_scheduled_jobs(self):
-        ScheduledJob = self.get_model('ScheduledJob')
-        jobs = ScheduledJob.objects.filter(
-            enabled=True, scheduled_time__lte=Now())
-        self.reschedule_jobs(jobs)
-
-    def reschedule_jobs(self, jobs):
+    def reschedule_jobs(self, model_name: str):
+        Model = self.get_model(model_name)
+        jobs = Model.objects.filter(enabled=True)
         for job in jobs:
             if job.is_scheduled() is False:
                 job.save()
