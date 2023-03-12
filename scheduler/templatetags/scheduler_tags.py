6ad9e72627e5927f4c357e645932cc6e35f2b9b6
Daniel M <daniel.maruani@gmail.com>
2023-03-12 14:07:56 -0400

    Fix error in django admin

--- scheduler/templatetags/scheduler_tags.py
+++ scheduler/templatetags/scheduler_tags.py
@@ -8,4 +8,4 @@ register = template.Library()
 @register.simple_tag()
 def scheduler_status(*args, **kwargs):
     sched = DjangoRQScheduler.instance()
-    return str(sched.status.value.upper())
+    return str(sched.status.value.upper()) if sched else "No internal scheduler"
