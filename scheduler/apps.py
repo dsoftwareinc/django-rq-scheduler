4fbb6f7717eda5a85d5ac12dd2f4ae599ce63f59
Daniel M <daniel.maruani@gmail.com>
2023-04-18 15:11:43 -0400

    Fix log level

--- scheduler/apps.py
+++ scheduler/apps.py
@@ -1,8 +1,6 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
-from scheduler.rq_classes import DjangoScheduler
-
 
 class SchedulerConfig(AppConfig):
     default_auto_field = 'django.db.models.AutoField'
@@ -10,4 +8,4 @@ class SchedulerConfig(AppConfig):
     verbose_name = _('Django RQ Scheduler')
 
     def ready(self):
-        DjangoScheduler.reschedule_all_jobs()
+        pass
